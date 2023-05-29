f172e1d540c156d889319680ce17ffe83379d90d
Rodrigo De Oliveira Rosa <Rodrigo.Rosa81@edu.pucrs.br>
2023-05-29 20:26:23 -0300

    criando projeto maven

--- src/main/java/com/bcopstein/SistCalculoFreteP6/Aplicacao/CustoBasicoTransporte.java
+++
@@ -1,5 +0,0 @@
-package com.bcopstein.SistCalculoFreteP6.Aplicacao;
-
-public record CustoBasicoTransporte(
-    String cidade,double valor){}
-
--- src/main/java/com/bcopstein/SistCalculoFreteP6/Aplicacao/SolicitaCustoDTO.java
+++
@@ -1,6 +0,0 @@
-package com.bcopstein.SistCalculoFreteP6.Aplicacao;
-
-public record SolicitaCustoDTO(
-    String cepOrigem, String cepDestino,int peso) {
-        
-    }
